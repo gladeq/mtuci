f0eb3056cb0fbc6cb83f022f8ef1ab7b7096c080
gladeq <voronkin.nickita2017@yandex.ru>
2024-09-29 22:50:22 +0300

    first commit

---
+++ src/main/java/mtuci/rbpomtuci/RbpomtuciApplication.java
@@ -0,0 +1,13 @@
+package mtuci.rbpomtuci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbpomtuciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbpomtuciApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mtuci/rbpomtuci/RbpomtuciApplicationTests.java
@@ -0,0 +1,13 @@
+package mtuci.rbpomtuci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbpomtuciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
